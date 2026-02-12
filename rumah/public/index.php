788a3735fa8f0515a14571e0dd2764248de6bbe3
alvithops <alvithops@gmail.com>
2026-02-12 15:40:58 +0700

    push 5

--- rumah/public/index.php
+++ rumah/public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Foundation\Application;
+use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
@@ -11,8 +11,14 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 
 require __DIR__.'/../vendor/autoload.php';
 
-$app = Application::configure(
-    basePath: dirname(__DIR__)
-)->create();
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
-$app->handleRequest(Request::capture());
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
